e998661e18011ef170c51a1cc9e488ff6b296a54
kawaMANMI <kawamam2007@gmail.com>
2026-04-14 12:12:12 +0100

    fe wline for import plot view added to views.py

--- inflammation/views.py
+++ inflammation/views.py
@@ -1,7 +1,11 @@
 """Module containing code for plotting inflammation data."""
 
-from matplotlib import pyplot as plt
+import matplotlib
 import numpy as np
+from matplotlib import pyplot as plt
+
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
 
 
 def visualize(data_dict):
